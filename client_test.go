d050f089c671d1421b4b8fc4789860baaca8ddc4
JustAdam <adambell7@gmail.com>
2014-05-28 13:28:35 +0200

    Test Twitter error response code handling.

--- client_test.go
+++ client_test.go
@@ -3,6 +3,10 @@
 package streamingtwitter
 
 import (
+	"github.com/garyburd/go-oauth/oauth"
+	"net/http"
+	"net/url"
+	"reflect"
 	"testing"
 )
 
@@ -44,5 +48,32 @@ func TestTwitterErrorOutput(t *testing.T) {
 
 	if err.Error() != "Error message (101)" {
 		t.Errorf("Expecting \"Error message (101)\", got %v", err)
+	}
+}
+
+func TestSendResponseErrorOutput(t *testing.T) {
+	client := NewClient()
+	errorCodes := []int{401, 403, 404, 406, 413, 416, 420}
+
+	for _, v := range errorCodes {
+		handler := func(*http.Client, *oauth.Credentials, string, url.Values) (*http.Response, error) {
+			resp := &http.Response{
+				StatusCode: v,
+			}
+			return resp, nil
+		}
+
+		testurl := &TwitterApiUrl{
+			AccessMethod:  "custom",
+			CustomHandler: handler,
+		}
+
+		_, err := client.sendRequest(testurl, &url.Values{})
+
+		if rerr, ok := err.(*TwitterError); !ok {
+			t.Errorf("Expecting TwitterError, got %v", reflect.TypeOf(err))
+		} else if rerr.Id != v {
+			t.Errorf("Expecting error ID %v, got %v", v, rerr.Id)
+		}
 	}
 }
